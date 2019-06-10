6669ccf26ec87d242f938190f279cb76a8e4be2a
Knut Ahlers <knut@ahlers.me>
2019-06-10 21:31:51 +0200

    Fix: After a restart force link is required
    Signed-off-by: Knut Ahlers <knut@ahlers.me>

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,7 +1,7 @@
 #!/usr/local/bin/dumb-init /bin/bash
 set -euxo pipefail
 
-ln dist/app.js app.js
+ln -f dist/app.js app.js
 korvike -i dist/index.html -o index.html
 
 exec python -m http.server 3000

c55d74ac3d1befc34e595a70a75c96fd64525910
Knut Ahlers <knut@ahlers.me>
2019-07-13 16:12:33 +0200

    Fix: For DTMF the numbers must be strings
    Signed-off-by: Knut Ahlers <knut@ahlers.me>

--- src/app.vue
+++ src/app.vue
@@ -61,7 +61,7 @@ export default {
 
   computed: {
     keys() {
-      return [1, 2, 3, 4, 5, 6, 7, 8, 9, '*', 0, '#']
+      return ['1', '2', '3', '4', '5', '6', '7', '8', '9', '*', '0', '#']
     },
 
     ongoingCall() {
